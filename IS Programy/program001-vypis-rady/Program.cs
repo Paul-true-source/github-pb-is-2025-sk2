3948c9c111e3ce613ef2cb0f2bc84d5bfff558a3
Pavel Bulíř <pavel.bulir@tul.cz>
2025-10-13 14:55:19 +0200

    Úkol 002 ‐ Git a GitHub ‐ základní příkazy #2

---
+++ IS Programy/program001-vypis-rady/Program.cs
@@ -0,0 +1,31 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("******************************************");
+    Console.WriteLine("*************Vypiš číselné řady***********");
+    Console.WriteLine("******************************************");
+    Console.WriteLine("******************************************");
+    Console.WriteLine("****************Pavel Loyan**************");
+    Console.WriteLine("*****************2. 10. 2025**************");
+    Console.WriteLine("******************************************");
+    Console.WriteLine("******************************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu, ale špatné řešení;
+    Console.Write("První číslo řady: ");
+    //int first = int.Parse(Console.ReadLine);
+
+    // Vstup hodnoty do programu, ale špatné řešení
+    Console.Write("hdf hdsfh jh");
+    int first;
+    while ((int.TryParseparse(Console.ReadLine())))
+
+    Console.WriteLine();
+    Console.WriteLine("Pro aplikování programu stiskni a");
+    again = Console.ReadLine();
+
+
+}
+
+
