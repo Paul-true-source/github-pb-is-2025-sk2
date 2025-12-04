73c19aa35aa72a1c5e01954b03ad7228346e0c7d
Pavel Bulíř <pavel.bulir@tul.cz>
2025-12-04 12:44:17 +0100

    #13 Základ Výpočet pi

---
+++ IS Programy/program011a-vypocet-pi/Program.cs
@@ -0,0 +1,30 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("*********** Výpis číselné řady *************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Pavel Bulíř ******************");
+    Console.WriteLine("************** 13.10.2025 ******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte první číslo řady (celé číslo): ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+    }
+
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+
+
+
+
+    
+}
