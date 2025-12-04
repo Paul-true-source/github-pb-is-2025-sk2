9efdbc427ad2b4179b59828d4bb50c4d9be4a478
Pavel Bulíř <pavel.bulir@tul.cz>
2025-12-04 13:36:35 +0100

    #13 konvergentni rady-DONE

--- IS Programy/program011a-vypocet-pi/Program.cs
+++ IS Programy/program011a-vypocet-pi/Program.cs
@@ -3,7 +3,7 @@ while (again == "a")
 {
     Console.Clear();
     Console.WriteLine("********************************************");
-    Console.WriteLine("*********** Výpis číselné řady *************");
+    Console.WriteLine("************* Výpis pořadí pi **************");
     Console.WriteLine("********************************************");
     Console.WriteLine("********************************************");
     Console.WriteLine("************* Pavel Bulíř ******************");
@@ -12,19 +12,42 @@ while (again == "a")
     Console.WriteLine("********************************************");
     Console.WriteLine();
 
-    Console.Write("Zadejte první číslo řady (celé číslo): ");
-    int first;
-    while (!int.TryParse(Console.ReadLine(), out first))
+    Console.Write("Zadejte přesnost pi (př.: 0,001): ");
+    double pres;
+    while (!double.TryParse(Console.ReadLine(), out pres)) //i = přesnost
     {
         Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
     }
 
+double i = 1;
+double piCtvrt = 1;
+double znamenko = 1;
+
+while((1/i)>=pres)
+    {
+        i = i+2;
+        znamenko = -znamenko;
+        piCtvrt = piCtvrt + znamenko*1/i;
+
+        Console.WriteLine("Zlomek: {0}/{1}; aktuální hodnota PI = {2}", znamenko, i, 4*piCtvrt);
+    }
+
+    double pi = 4 * piCtvrt;
+
+    Console.WriteLine("Přesnost PI: {0}", pi); 
+
+
+
+
+
+
+
+
+
+
+
+
     Console.WriteLine("Pro opakování programu stiskněte klávesu a");
     again = Console.ReadLine();
 
-
-
-
-
-    
 }
