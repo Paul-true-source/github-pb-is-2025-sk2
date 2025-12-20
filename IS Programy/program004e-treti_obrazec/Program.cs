2ff0ae53249fe630ee9650f30079ddba92e9e70f
Pavel Bulíř <pavel.bulir@tul.cz>
2025-12-21 00:06:14 +0100

    #7 třetí obrazec - dodělání poznámek

--- IS Programy/program004e-treti_obrazec/Program.cs
+++ IS Programy/program004e-treti_obrazec/Program.cs
@@ -26,24 +26,24 @@ while (again == "a")
         for(int sloupec = 1; sloupec <= velikost; sloupec++)
         {
             
-            if((radek-1)/2 % 2 == 0)
+            if((radek-1)/2 % 2 == 0) // bere v potaz 1-2, 5-6, 9-11,..... řádek => uděláme pruhovaný
             {
-                if ((sloupec + 1)/2%2 ==0)
+                if ((sloupec + 1)/2%2 ==0) // z toho co máme vybraný uděláme pruhovaný => rozdělíme jakoby na čtverečky
                 {
-                    Console.Write(" ");
+                    Console.Write(" "); //hlavní znak
                 }
                 else
                 {
-                    Console.Write("*");
+                    Console.Write("*"); // výplň
                 }
             }
-            else if ((sloupec + 1)/2%2 ==0)
+            else if ((sloupec + 1)/2%2 ==0) // nemám nejmenší tušení ale asi něco s pruhováním, asi jako že se to bude křížit s tím prvním a nebude se to navzájem rušit ¯\_(ツ)_/¯
             {
-                Console.Write("*");
+                Console.Write("*"); //hlavní znak
             }
             else
             {
-                Console.Write(" ");
+                Console.Write(" "); // výplň
             }
 
             
@@ -55,7 +55,7 @@ while (again == "a")
     }
 
 
-Console.WriteLine();
+    Console.WriteLine();
     Console.WriteLine("Pro opakování programu stiskněte klávesu a");
     again = Console.ReadLine();
 }
